82346750b297fdbd7f4f56b679ee5246010b03fc
citrusarc <citrusarc.studio@gmail.com>
2025-10-21 10:40:48 +0800

    update navbar

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -11,7 +11,7 @@ import { siteConfig } from "@/config/site";
 
 export default function Navbar() {
   const pathname = usePathname();
-  const [scroll, setScroll] = useState(false);
+  // const [scroll, setScroll] = useState(false);
   const [openMenu, setOpenMenu] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
 
@@ -19,13 +19,13 @@ export default function Navbar() {
   const isHome = pathname === "/";
   const isWhatCustomersSay = pathname === "/what-customers-say";
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setScroll(window.scrollY > 50);
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     setScroll(window.scrollY > 50);
+  //   };
+  //   window.addEventListener("scroll", handleScroll);
+  //   return () => window.removeEventListener("scroll", handleScroll);
+  // }, []);
 
   useEffect(() => {
     document.body.style.overflow = openMenu ? "hidden" : "";
@@ -48,12 +48,12 @@ export default function Navbar() {
   return (
     <nav
       className={clsx(
-        "sticky top-0 z-50 flex p-4 sm:py-6 sm:px-24 w-full items-center justify-between transition-colors duration-300",
-        openMenu
-          ? "text-black bg-white"
-          : scroll
-          ? "text-white bg-violet-600"
-          : !(isHome || isWhatCustomersSay)
+        "sticky top-0 z-50 flex p-4 sm:py-4 sm:px-24 w-full items-center justify-between transition-colors duration-300",
+        // openMenu
+        //   ? "text-black bg-white"
+        // : scroll
+        // ? "text-white bg-violet-600"
+        !(isHome || isWhatCustomersSay)
           ? "text-black bg-white"
           : "text-white bg-transparent"
       )}
@@ -65,27 +65,27 @@ export default function Navbar() {
           <Menu className="w-6 h-6" />
         )}
       </button>
-      <Link href="/" className={clsx(openMenu && "invisible")}>
+      <Link href="/">
         <BrandLogo className="w-16 sm:w-24 h-8 sm:h-12" />
       </Link>
-      <ShoppingBag className={clsx("w-6 h-6", openMenu && "invisible")} />
+      <ShoppingBag className="w-6 h-6" />
       {openMenu && (
         <div
           ref={menuRef}
           className={clsx(
-            "absolute top-full left-0 p-4 sm:py-6 sm:px-24 w-full h-screen sm:h-fit shadow-md text-black bg-white transform transition-all duration-300 origin-top",
+            "absolute top-full left-1/2 -translate-x-1/2 p-4 w-[94vw] max-w-[2400px] h-fit shadow-md rounded-2xl sm:rounded-4xl overflow-hidden backdrop-blur-2xl text-black bg-white/80 transform transition-all duration-300 origin-top",
             openMenu
               ? "opacity-100 scale-y-100 pointer-events-auto"
               : "opacity-0 scale-y-0 pointer-events-none"
           )}
         >
-          <div className="flex flex-col gap-8">
+          <div className="flex flex-col gap-4">
             {navItems.map((item) => (
               <Link
                 key={item.id}
                 href={item.href}
                 onClick={() => setOpenMenu(false)}
-                className="p-4 w-fit text-xl sm:text-2xl hover:text-white hover:bg-violet-600"
+                className="p-4 w-fit rounded-2xl overflow-hidden text-xl hover:text-white hover:bg-violet-600"
               >
                 {item.name}
               </Link>
