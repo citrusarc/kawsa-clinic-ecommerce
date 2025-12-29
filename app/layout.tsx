da8765c14d761b5a186e10593ce1f33dd793bdfc
citrusarc <citrusarc.studio@gmail.com>
2025-12-29 18:37:05 +0800

    add cron

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import "@/app/globals.css";
 import BrandLogo from "@/components/icons/BrandLogo";
 import Navbar from "@/components/ui/Navbar";
 import { siteConfig } from "@/config/site";
+import "./utils/sendPendingEmailsCron";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://kawsa-clinic-ecommerce.vercel.app"),
