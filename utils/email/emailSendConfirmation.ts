def204ce949b6ff7cd2e7c746d996827acdc59e5
citrusarc <citrusarc.studio@gmail.com>
2026-01-06 14:18:10 +0800

    update emailSendConfirmationTemplate

--- utils/email/emailSendConfirmation.ts
+++ utils/email/emailSendConfirmation.ts
@@ -3,50 +3,70 @@ import { EmailSendConfirmationTemplateProps } from "@/types";
 export function emailSendConfirmationTemplate({
   orderNumber,
   fullName,
-  courierName,
-  trackingUrl,
-  awbNumber,
   items,
   subTotalPrice,
   shippingFee,
   totalPrice,
 }: EmailSendConfirmationTemplateProps) {
   return `
-  <div style="background-color:#f4f4f4; padding:24px; font-family:Arial;">
-    <h2>Order Confirmed 🎉</h2>
+  <div style="background-color:#f4f4f4; padding:24px; font-family:Arial, sans-serif;">
+  <img 
+    src="https://kawsa-clinic-ecommerce.vercel.app/Images/brand-logo-black.png" 
+    alt="Kawsa MD Formula Logo" 
+    style="width:120px; height:auto; margin-bottom:16px;" 
+  />
 
-    <p>Hi ${fullName},</p>
+  <h2>
+    Your Order Has Been Confirmed!
+  </h2>
 
-    <p>
-      Your order <strong>${orderNumber}</strong> has been shipped.
-    </p>
+  <p>Hi ${fullName},</p>
+  <p>
+    Thank you for your purchase!<br/>
+    We’re happy to let you know that your order 
+    <span style="font-weight:bold;">${orderNumber}</span> has been successfully confirmed.
+  </p>
 
-    <p>
-      <strong>Courier:</strong> ${courierName}<br/>
-      <strong>Tracking Number:</strong>
-      ${
-        trackingUrl && awbNumber
-          ? `<a href="${trackingUrl}" target="_blank">${awbNumber}</a>` // //
-          : awbNumber || "-" // //
-      }
-    </p>
+  <hr style="margin:16px 0; border:none; border-top:1px solid #ccc;" />
 
-    <hr />
-
-    <h3>Order Summary</h3>
-    <ul>
-      ${items
-        .map(
-          (item) => `<li>${item.itemName} × ${item.itemQuantity}</li>` // //
-        )
-        .join("")}
-    </ul>
-
-    <p>Sub Total: RM ${subTotalPrice.toFixed(2)}</p>
-    <p>Shipping Fee: RM ${shippingFee.toFixed(2)}</p>
-    <p><strong>Total: RM ${totalPrice.toFixed(2)}</strong></p>
-
-    <p>Thank you for shopping with us ❤️</p>
+  <h3 >Order Summary</h3>
+  <div>
+    ${items
+      .map(
+        (item) => `
+      <div style="display:flex; gap:12px; margin-bottom:12px; align-items:flex-start;">
+        <div style="width:80px; height:80px; overflow:hidden; border-radius:12px; flex-shrink:0;">
+          <img src="${item.itemSrc}" alt="${
+          item.itemName
+        }" style="width:100%; height:100%; object-fit:cover;">
+        </div>
+        <div>
+          <p style="margin:0; font-weight:bold;">${item.itemName}</p>
+          <p style="margin:0; color:#6b7280;">Quantity: ${item.itemQuantity}</p>
+          <p style="margin:0; color:#7c3aed; font-weight:bold;">RM${item.itemTotalPrice.toFixed(
+            2
+          )}</p>
+        </div>
+      </div>
+    `
+      )
+      .join("")}
   </div>
+
+  <div style="margin-top:16px; border-top:1px solid #ccc; padding-top:16px;">
+    <p style="margin:0; color:#6b7280;">Sub Total: RM ${subTotalPrice.toFixed(
+      2
+    )}</p>
+    <p style="margin:0; color:#6b7280;">Shipping Fee: RM ${shippingFee.toFixed(
+      2
+    )}</p>
+    <p style="margin-top:12px; font-size:18px; font-weight:bold;">Total: RM ${totalPrice.toFixed(
+      2
+    )}</p>
+  </div>
+
+  <p style="margin-top:16px;">Thank you for shopping with us️</p>
+</div>
+
   `;
 }
