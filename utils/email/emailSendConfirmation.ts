1e645ab472530d9cc80ce7ab484673961e1be4a4
citrusarc <citrusarc.studio@gmail.com>
2026-01-07 14:10:42 +0800

    update email footer

--- utils/email/emailSendConfirmation.ts
+++ utils/email/emailSendConfirmation.ts
@@ -90,8 +90,8 @@ export function emailSendConfirmationTemplate({
     </div>
     
   </div>
-  <div style="padding: 0 96px;">
-  <p style="margin: 24px 0 0 0; font-size: 14px;">Need help? Reach out to us at <a href="mailto:drkay.skincare@gmail.com" style="color: #0000EE; text-decoration: none;">drkay.skincare@gmail.com</a> or contact us via Whatsapp at <a href="https://wasap.my/60182074771" style="color: #0000EE; text-decoration: none;">+6018-2074771</a> / <a href="https://wasap.my/60138088912" style="color: #0000EE; text-decoration: none;">+6013-8088912</a></p>
+  <div style="max-width: 600px; margin: 16px auto 0 auto; padding: 0;">
+  <p style="font-size: 14px;">Need help? Reach out to us at <a href="mailto:drkay.skincare@gmail.com" style="color: #0000EE; text-decoration: none;">drkay.skincare@gmail.com</a> or contact us via Whatsapp at <a href="https://wasap.my/60182074771" style="color: #0000EE; text-decoration: none;">+6018-2074771</a> / <a href="https://wasap.my/60138088912" style="color: #0000EE; text-decoration: none;">+6013-8088912</a></p>
 </div></div>
   `;
 }
